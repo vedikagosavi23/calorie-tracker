bc00101c55a47970ca148cd6e4d8084cef1f2609
Rohit1034 <rohit.patil1034@gmail.com>
2025-07-05 01:55:24 +0530

    increase image size

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const path = require('path');
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: '5mb' }));
+app.use(express.json({ limit: '10mb' }));
 const analyzeImageHandler = require('./api/analyze-image');
 
 // Health check endpoint
